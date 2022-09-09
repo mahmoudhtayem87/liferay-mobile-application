21a554cc3f928cd1cd01a2761435e9357ce17cf4
Mahmoud Hussein Tayem <77802068+mahmoudhtayem87@users.noreply.github.com>
2022-09-10 01:27:21 +0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Liferay Mobile Application',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
